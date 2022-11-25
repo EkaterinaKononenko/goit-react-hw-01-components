628911a64124f49571de0b7e06bcdf57b249d27d
EkaterinaKononenko <kaktysik4@ukr.net>
2022-11-25 10:38:58 +0200

    add random color hover

--- src/components/Statistics/Statistics.styled.js
+++ src/components/Statistics/Statistics.styled.js
@@ -20,11 +20,18 @@ export const StatList = styled.ul`
   padding: ${props => props.theme.space[0]}px;
 `;
 
+const getRandom = () => {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+};
+
 export const StatItem = styled.li`
   display: flex;
   flex-direction: column;
   align-items: center;
   padding: ${props => props.theme.space[1]}px;
+  &:hover {
+    background-color: ${getRandom};
+  }
 `;
 
 export const StatLabel = styled.span`
